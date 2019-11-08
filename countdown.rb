c840d9c520d3089ce2a5534331f187814719f2a7
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-08 02:18:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,3 +2,18 @@
 
 def countdown
 end
+
+
+
+
+
+
+# countdown_output = 0
+# def countdown(10)
+#   while countdown_output < 10
+#     puts "#{number} SECOND(S)!"
+#     countdown_output += 1
+#     return "HAPPY NEW YEAR!"
+#
+#   end
+# end
