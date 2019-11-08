8639111328ab36522a586b0e6ee2657e392b6fe1
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-08 02:37:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,14 @@
 #write your code here
 
-def countdown
-  countdown_output -= 1
-    puts "#{number} SECOND(S)!"
+def countdown(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
 end
 
 
